312cf0d577c058e6ef279dd88d8b64cfd489a9f2
nate-branson <nb1288@messih.edu>
2022-03-14 00:09:16 -0400

    added main.py to .gitignore

--- main.py
+++ main.py
@@ -8,18 +8,25 @@ from read_files import *
 from analysis import *
 
 def main():
-    game = "Example"
-    date = "Jan1"
-    data, _     = read_file('data/' + game + '/' + game + '.csv')
+    game        = "Kings"
+    date        = "Mar2"
+    data, _     = read_file('../Messiah_Shot/' + game + '/' + date + '_Shot_' + game + '.csv')
     shooters    = data['shooter']
     players = []
     for i in shooters:
         if not i in players:
             players.append(i)
+    #players.append(-1)
     
+    #data = read_multi_files('../Messiah_Shot/Shot_Charts')
+    #print(data)
     for i in players:
         plot_shot_locs(data, player = i, game = game, date = date, extra = True)
     plot_shot_locs(data, player = -1, game = game, date = date)
+    
+
+    #plot_goal_locs(data)
+
 
 if __name__ == '__main__':
     main()
